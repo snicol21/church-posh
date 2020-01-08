59a5a05cf110579d85ca1fac81e9cdfd4e892eca
Spencer Nicol <spencer.nicol@healthcatalyst.com>
2020-01-08 09:58:12 -0700

    add launch json and required version for publishing

--- scripts/church-posh.deploy.ps1
+++ scripts/church-posh.deploy.ps1
@@ -1,9 +1,11 @@
-param (
-    [Parameter(Mandatory = $true)]
-    [string]$apiKey
-)
+# param (
+#     [Parameter(Mandatory = $true)]
+#     [string]$apiKey
+# )
 $Root = $(Get-Item $PSScriptRoot).Parent.FullName;
 $ModulePath = "$Root\church-posh";
+$ManifestPath = "$ModulePath\church-posh.psd1"
+$Manifest = Import-PowerShellDataFile -Path $ManifestPath
 $env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)$ModulePath"
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-Publish-Module -Name 'church-posh' -NuGetApiKey $apiKey -Path $ModulePath
+Publish-Module -Name 'church-posh' -NuGetApiKey $apiKey -RequiredVersion $Manifest.ModuleVersion
